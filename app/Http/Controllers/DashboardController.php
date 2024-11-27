c9a6bd6e65f9dc7cc25a6709e1e59c1691369c0b
Elbek Shukurullayev <vermino@Elbeks-MacBook-Pro.local>
2024-11-27 14:20:32 +0500

    fix login route

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller; // Add this line
 use App\Models\Bot;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
+    /**
+     * Show the application dashboard.
+     */
     public function index()
     {
         $user = Auth::user();

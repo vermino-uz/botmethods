535fe448a56ead6b59f24ea5b62058276d6d27df
Elbek Shukurullayev <vermino@Elbeks-MacBook-Pro.local>
2024-11-27 18:12:00 +0500

    fix login route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -274,7 +274,7 @@
             <!-- Sidebar -->
             <div class="sidebar">
                 <div class="px-3 mb-3">
-                    <a href="{{ route('bots.create') }}" class="btn btn-primary w-100">
+                    <a href="{{ route('bots.create') }}" class="btn btn-primary w-100" style="background-color: #87CEFA; border-color: #87CEFA; color: #000;">
                         <i class="bi bi-plus-lg me-2"></i>New Bot
                     </a>
                 </div>
